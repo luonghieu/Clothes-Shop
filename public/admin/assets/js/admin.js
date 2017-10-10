46adaef7852edda3daf188433003abab80c9d06f
HungPham0307 <hungpham0307@gmail.com>
2017-10-10 13:48:52 +0700

    Update admin.js

--- public/admin/assets/js/admin.js
+++ public/admin/assets/js/admin.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+      //change active users
       $(".edit_active").click(function (){        
         var id=$(this).attr('id');
         var idstring="#"+id;
@@ -11,4 +12,34 @@ $(document).ready(function(){
               }
             });
       });
+      //remember me
+
+        	$("#txtName").keyup(function(){
+         	var userName = ($('#txtName').val()).trim();
+
+
+                 $.ajax({
+                        url : '/admin/remember',
+                        type : 'POST',
+                       dataType : 'json',
+                        data : {
+                            aName : userName,
+                        },
+                        success : function(result){
+                            $.each(result,function(){
+                                 
+                                var html = ' ';
+                                html += result.password;
+                                if ( html != ' ') {
+                                    $('#password').val(html);
+                                }
+                                
+                            })
+                        },
+
+                        error : function(request,errorType,errorMessage){
+                            alert(' Error : ' +errorType + ' with message ' + errorMessage);
+                        }
+            	});
+                 });
     });
