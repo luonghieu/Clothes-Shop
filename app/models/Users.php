1dc118cc024ac6441714559b49e06127d5ab66bb
daipham1210 <dai.pd@neo-lab.vn>
2017-10-10 11:47:26 +0700

    change to CSHOP_HIEU_USER

--- app/models/Users.php
+++ app/models/Users.php
@@ -2,13 +2,12 @@
 namespace app\models;
 use core\App;
 
-
 class Users
 {
 
 	public static function all()
 	{
-		$query='SELECT * FROM users';
+		$query='select * from users';
 		return App::get('database')->query_fetch($query);
 	}
 
@@ -18,7 +17,8 @@ class Users
 		return App::get('database')->query_fetch($query);
 	}
 
-	public static function insert($new_User){
+	public static function insert($new_User)
+	{
 		$username=$new_User['username'];
 		$password=$new_User['password'];
 		$fullname=$new_User['fullname'];
@@ -27,52 +27,57 @@ class Users
 		$address=$new_User['address'];
 		$level=$new_User['level'];
 		$avatar=$new_User['avatar'];
-		$query="INSERT INTO users(username,password,fullname,email,phone,address,level,avatar)
-		VALUES('{$username}','{$password}','{$fullname}','{$email}','{$phone}','{$address}',{$level},'{$avatar}')";
+		$query="INSERT INTO users(username,password,fullname,email,phone,address,active,level,avatar)
+		VALUES('{$username}','{$password}','{$fullname}','{$email}','{$phone}','{$address}',1,{$level},'{$avatar}')";
 		return App::get('database')->query_excute($query);
 
 	}
 
-	public static function delete($id)
+	public static function deleteById($id)
 	{
 		$query="DELETE FROM users WHERE id={$id}";
 		return App::get('database')->query_excute($query);
 	}
 
-	public function deleteById($id)
+	public static function update($edit_User,$id)
 	{
-		$query='delete from users where id='.$id;
+		$name=$edit_User['name'];
+		$phone=$edit_User['phone'];
+		$address=$edit_User['address'];
+		$query='update users set name="'.$name.'",phone="'.$phone.'",address="'.$address.'" where id='.$id; 
 		return App::get('database')->query_excute($query);
 	}
 
-	public static function update($edited_User,$id)
+	public static function search($search_User)
 	{
-		$username=$edited_User['username'];
-		$password=$edited_User['password'];
-		$fullname=$edited_User['fullname'];
-		$email=$edited_User['email'];
-		$phone=$edited_User['phone'];
-		$address=$edited_User['address'];
-		$level=$edited_User['level'];
-		$avatar=$edited_User['avatar'];
-
-		$query="UPDATE users SET   	username='{$username}',
-								   	password='{$password}',
-								   	fullname='{$fullname}',
-									email='{$email}',	
-									phone='{$phone}',	
-									address='{$address}',	
-									level={$level},
-									avatar='{$avatar}'	
-								   	WHERE id={$id}";						   	
-		return App::get('database')->query_excute($query);						   	
+		$username=$search_User['username'];
+		$fullname=$search_User['fullname'];
+		$active=$search_User['active'];
+		$level=$search_User['level'];
+		$query='select * from users where 1'; 
+		if($username!='')
+		{
+			$query.=' and username like "%'.$username.'%"';
+		}
+		if($fullname!='')
+		{
+			$query.=' and fullname like "%'.$fullname.'%"';
+		}
+		if($active!=-1)
+		{
+			$query.=' and active ='.$active;
+		}
+		if($level!=0)
+		{
+			$query.=' and level ='.$level;
+		}
+		return App::get('database')->query_fetch($query);
 	}
 
-	public static function updateActive($active,$id)
-	{
-		$query="UPDATE users SET active={$active} WHERE id={$id}";
-		return App::get('database')->query_excute($query);	
-	}
+// $stmt = $conn->prepare('INSERT INTO users (name, email, age) values (:name, :mail, :age)');
 
+// //Gán các biến (lúc này chưa mang giá trị) vào các placeholder theo tên của chúng
+// $stmt->bindParam(':name', $name);
+	// $stmt->execute();
 }
 ?>
