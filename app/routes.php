56e802dc85b915dfe107e74ce27e1b376845ca12
luonghieu <luonghieu.t3@gmail.com>
2017-10-10 08:22:11 +0700

    first commit

---
+++ app/routes.php
@@ -0,0 +1,14 @@
+<?php 
+
+$router->get('admin/users','UsersController@index');
+$router->get('admin/users/add','UsersController@add');
+$router->post('admin/users/add','UsersController@store');
+$router->get('admin/users/edit','UsersController@edit');
+$router->get('admin/users/delete','UsersController@destroy');
+$router->post('admin/users/search','UsersController@search');
+
+
+$router->get('*','ErrorController@error');
+$router->post('*','ErrorController@error');
+
+?>
---
+++ app/views/admin/users/index.view.php
@@ -0,0 +1,303 @@
+<?php
+
+require dirname(__DIR__,1).'/require/header.view.php';
+?>
+<div class="main-container ace-save-state" id="main-container">
+	<script type="text/javascript">
+		try{ace.settings.loadState('main-container')}catch(e){}
+	</script>
+	<?php 
+	require dirname(__DIR__,1).'/require/leftbar.view.php';
+	?>
+	<div class="main-content">
+		<div class="main-content-inner">
+			<div class="breadcrumbs ace-save-state" id="breadcrumbs">
+				<ul class="breadcrumb">
+					<li>
+						<i class="ace-icon fa fa-home home-icon"></i>
+						<a href="#">Home</a>
+					</li>
+					<li class="active">Dashboard</li>
+				</ul><!-- /.breadcrumb -->
+
+				<div class="nav-search" id="nav-search">
+					<form class="form-search">
+						<span class="input-icon">
+							<input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+							<i class="ace-icon fa fa-search nav-search-icon"></i>
+						</span>
+					</form>
+				</div><!-- /.nav-search -->
+			</div>
+
+			<div class="page-content">
+				<div class="ace-settings-container" id="ace-settings-container">
+					<div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
+						<i class="ace-icon fa fa-cog bigger-130"></i>
+					</div>
+
+					<div class="ace-settings-box clearfix" id="ace-settings-box">
+						<div class="pull-left width-50">
+							<div class="ace-settings-item">
+								<div class="pull-left">
+									<select id="skin-colorpicker" class="hide">
+										<option data-skin="no-skin" value="#438EB9">#438EB9</option>
+										<option data-skin="skin-1" value="#222A2D">#222A2D</option>
+										<option data-skin="skin-2" value="#C6487E">#C6487E</option>
+										<option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
+									</select>
+								</div>
+								<span>&nbsp; Choose Skin</span>
+							</div>
+
+							<div class="ace-settings-item">
+								<input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-add-container" autocomplete="off" />
+								<label class="lbl" for="ace-settings-add-container">
+									Inside
+									<b>.container</b>
+								</label>
+							</div>
+						</div><!-- /.pull-left -->
+
+						<div class="pull-left width-50">
+
+							<div class="ace-settings-item">
+								<div class="pull-left">
+									<select id="skin-colorpicker" class="hide">
+										<option data-skin="no-skin" value="#438EB9">#438EB9</option>
+										<option data-skin="skin-1" value="#222A2D">#222A2D</option>
+										<option data-skin="skin-2" value="#C6487E">#C6487E</option>
+										<option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
+									</select>
+								</div>
+								<span>&nbsp; Choose Skin</span>
+							</div>
+							<div class="ace-settings-item">
+								<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact" autocomplete="off" />
+								<label class="lbl" for="ace-settings-compact"> Compact Sidebar</label>
+							</div>
+
+							<div class="ace-settings-item">
+								<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-highlight" autocomplete="off" />
+								<label class="lbl" for="ace-settings-highlight"> Alt. Active Item</label>
+							</div>
+						</div><!-- /.pull-left -->
+					</div><!-- /.ace-settings-box -->
+				</div><!-- /.ace-settings-container -->
+
+				<div class="page-header">
+					<h1 style="font-weight: bold">
+						Users
+								<!-- <small>
+									<i class="ace-icon fa fa-angle-double-right"></i>
+									Static &amp; Dynamic Tables
+								</small> -->
+							</h1>
+						</div><!-- /.page-header -->
+
+						<div class="row">
+							<div class="col-xs-12">
+								<!-- PAGE CONTENT BEGINS -->
+								<div class="row">
+									<div class="col-xs-12">
+										<a href="/admin/users/add" class="btn btn-success 	fa fa-plus-square fa-lg" title=""> Add User</a>
+									</br>
+								</br>
+								<h1>
+									<?php
+									if(isset($_GET['msg'])){
+										echo $_GET['msg'];
+									}
+									?>
+								</h1>
+
+
+								<div class="row text-center">
+									<div class="col-xs-12">
+										<!-- PAGE CONTENT BEGINS -->
+										<form class="form-horizontal" role="form" action="/admin/users/search" method="post">
+                                            <?php
+                                            $username='';
+                                            $fullname='';
+                                            $active=-1;
+                                            $level=0;
+                                            if(isset($search_User))
+                                            {
+                                                $username=$search_User['username'];
+                                                $fullname=$search_User['fullname'];
+                                                $active=$search_User['active'];
+                                                $level=$search_User['level'];
+                                            }
+                                            ?>
+                                            <div class="row">
+                                              <div class="col-xs-3">
+                                                 <div class="form-group">
+                                                   <div class="pos-rel">
+                                                      <input class="typeahead scrollable" name="username" type="text" placeholder="Username" value="<?php echo $username; ?>" />
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div class="col-xs-3">
+                                             <div class="form-group">
+                                               <div class="pos-rel">
+                                                  <input class="typeahead scrollable" name="fullname" type="text" value="<?php echo $fullname; ?>" placeholder="Fullname" />
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-xs-2">
+                                         <div class="form-group">
+                                           <select id="active" name="active" class="multiselect">
+                                              <option <?php if($active==-1) echo 'selected="selected"';?> value="-1">--Active--</option>
+                                              <option <?php if($active==1) echo 'selected="selected"';?> value="1">Active</option>
+                                              <option <?php if($active==0) echo 'selected="selected"';?> value="0">Non-Active</option>
+                                          </select>
+                                      </div>
+                                  </div>
+                                  <div class="col-sm-2">
+                                     <div class="form-group">
+                                       <select id="level" name="level" class="multiselect" >
+                                          <option <?php if($level==0) echo 'selected="selected"';?> value="0">--Level--</option>
+                                          <option <?php if($level==1) echo 'selected="selected"';?> value="1">Admin</option>
+                                          <option <?php if($level==2) echo 'selected="selected"';?> value="2">Employees</option>
+                                          <option <?php if($level==3) echo 'selected="selected"';?> value="3">Customer</option>
+                                      </select>
+                                  </div>
+                              </div>
+                              <div class="col-xs-2">
+                                <div class="form-group">
+                                    <button class="btn btn-success  fa fa-plus-square fa-lg" type="submit" name="search">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- PAGE CONTENT ENDS -->
+                </div><!-- /.col -->
+            </div>
+
+            <table id="simple-table" class="table  table-bordered table-hover">
+               <thead>
+                  <tr>
+                     <th class="text-center">
+                        Id
+                    </th>
+                    <th class="text-center">
+                        Image
+                    </th>
+                    <th class="text-center">
+                        Username
+                    </th>
+                    <th class="text-center">
+                        Fullname
+                    </th>
+                    <th class="text-center">
+                        Phone
+                    </th>
+                    <th class="text-center">
+                        Address
+                    </th>
+                    <th class="text-center">
+                        Level
+                    </th>
+                    <th class="text-center">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+              <?php 
+              if(!empty($users))
+              {
+                foreach($users as $item){
+                 $id=$item->id;
+                 $username=$item->username;
+                 $fullname=$item->fullname;
+                 $phone=$item->phone;
+                 $address=$item->address;
+                 $level=$item->level;
+                 $avatar=$item->avatar;
+                 ?>
+                 <tr>
+                    <td class="text-center">
+                       <?php echo $id;?>
+                   </td>
+                   <td class="text-center">
+                       <img class="avatarintable" 
+                       src="/public/admin/upload/avatar/<?php
+                       if($avatar==''){
+                          echo "default.jpg";
+                      }else{
+                          echo $avatar;
+                      }
+                      ?>" alt="">
+                  </td>
+                  <td class="text-center">
+                   <?php echo $username;?>
+               </td>
+               <td class="text-center">
+                   <?php echo $fullname;?>
+               </td>
+               <td class="text-center">
+                   <?php echo $phone;?>
+               </td>
+
+               <td class="text-center">
+                   <?php echo $address;?>
+               </td>
+
+               <td class="text-center">
+                   <?php
+                   if($level==1){
+                      echo "admin";
+                  }else if($level==2){
+                      echo "employee";
+                  }else{
+                      echo "customer";
+                  }
+                  ?>
+              </td>
+              <td class="text-center">
+               <div class="hidden-sm hidden-xs btn-group">
+                <a class="btn btn-xs btn-info" href="/admin/users/edit?id=<?php echo $id; ?>">
+                    <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </a>
+                <a class="btn btn-xs btn-danger" href="/admin/users/delete?id=<?php echo $id; ?>">
+                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </a>
+            </div>
+        </td>
+    
+    <?php
+}
+}else{
+  ?>
+  <td class="text-center" colspan="8">No data</td>
+<?php
+}
+?>
+</tr>
+</tbody>
+</table>
+</div><!-- /.span -->
+</div><!-- /.row -->
+
+<div class="row text-center">
+    <ul class="pagination">
+        <li><a href="#">1</a></li>
+        <li class="active"><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+    </ul>
+</div>
+
+<!-- PAGE CONTENT ENDS -->
+</div><!-- /.col -->
+</div><!-- /.row -->
+</div><!-- /.page-content -->
+</div>
+</div><!-- /.main-content -->
+
+<?php 
+require dirname(__DIR__,1).'/require/footer.view.php';
+?>
