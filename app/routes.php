4392e0f0cddf0ba07abf3e146023760186560fba
HungPham0307 <hungpham0307@gmail.com>
2017-10-10 13:45:34 +0700

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -3,19 +3,24 @@
 $router->get('admin/users','UsersController@index');
 $router->get('admin/users/add','UsersController@add');
 $router->post('admin/users/add','UsersController@store');
-<<<<<<< HEAD
 $router->get('admin/users/edit/{id}','UsersController@edit');
 $router->post('admin/users/edit/{id}','UsersController@update');
 $router->get('admin/users/delete','UsersController@destroy');
 $router->post('admin/users/search','UsersController@search');
 $router->get('admin/users/active','UsersController@changeActive');
-
-$router->get('*','ErrorController@error');
-$router->post('*','ErrorController@error');
-=======
 $router->get('admin/users/edit','UsersController@edit');
 $router->get('admin/users/delete','UsersController@destroy');
 
->>>>>>> CSHOP_DAI_USERS
+$router->get('admin/login','AuthController@getLogin');
+$router->post('admin/login','AuthController@postLogin');
+$router->post('admin/remember','AuthController@ajaxRemember');
+$router->get('admin/logout','AuthController@getLogout');
+
+$router->get('*','ErrorController@error');
+$router->post('*','ErrorController@error');
+
+
+
+
 
 ?>
