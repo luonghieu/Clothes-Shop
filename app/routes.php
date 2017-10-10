f07bee390ee63ce2925ae03041626a43abd625ec
Mai-Vo <mai.vv@neo-lab.vn>
2017-10-10 10:51:07 +0700

    + add edit delete users

--- app/routes.php
+++ app/routes.php
@@ -7,7 +7,7 @@ $router->get('admin/users/edit/{id}','UsersController@edit');
 $router->post('admin/users/edit/{id}','UsersController@update');
 $router->get('admin/users/delete','UsersController@destroy');
 $router->post('admin/users/search','UsersController@search');
-
+$router->get('admin/users/active','UsersController@changeActive');
 
 $router->get('*','ErrorController@error');
 $router->post('*','ErrorController@error');
